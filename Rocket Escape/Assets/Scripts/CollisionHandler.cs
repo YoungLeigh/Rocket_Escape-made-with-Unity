8e258ad3c6604fba21a1e02f87d4209a1f129b15
YoungLeigh <yoonyoung.lee1218@gmail.com>
2023-01-06 23:14:56 +0900

    .

--- Rocket Escape/Assets/Scripts/CollisionHandler.cs
+++ Rocket Escape/Assets/Scripts/CollisionHandler.cs
@@ -1,22 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 1.3f;
     private void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
         {
             case "Friendly":
-                Debug.Log("friendly");
-                    break;
+                break;
             case "Finish":
-                Debug.Log("you finished!");
-                    break;
+                StartSucessSequence();
+                break;
             default:
-                Debug.Log("You lost");
+                StartCrashSequence();
                 break;
         }
+    }
+    
+    void StartSucessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex+ 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
